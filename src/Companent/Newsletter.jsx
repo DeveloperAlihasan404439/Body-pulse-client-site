1f4662357e0be49bcde215c6cb29b27eb4dea1e7
alihasanlitonislam <alihasan404439@gmail.com>
2023-12-13 17:46:09 +0600

    updated the projact in the github

--- src/Companent/Newsletter.jsx
+++ src/Companent/Newsletter.jsx
@@ -1,122 +1,101 @@
-import Swal from "sweetalert2";
-import useAxiosPublick from "../Sheard/Hooks/useAxiosPublick";
 import SectionTitle from "../Sheard/SectionTitle/SectionTitle";
 import "./Newsletter.css";
-import useAuth from "../Sheard/Hooks/useAuth";
 const Newsletter = () => {
-  const { user } = useAuth();
-  const axiosPublick = useAxiosPublick();
-  const hendelSubscribe = (e) => {
-    e.preventDefault();
-    const target = e.target;
-    const name = target.name.value;
-    const email = target.email.value;
-    const userInfo = {
-      name,
-      email,
-    };
-    axiosPublick.post("/subscribers", userInfo).then((res) => {
-      if (res.data.insertedId) {
-        target.reset();
-        Swal.fire({
-          position: "center",
-          icon: "success",
-          title: "success the subscribe now",
-          showConfirmButton: false,
-          background: "#5b1ae9",
-          color: "#FDFDFD",
-          timer: 2000,
-        });
-      }
-    });
-  };
   return (
     <>
       <SectionTitle heading="Subscribers" title="Subscribers Newsletter " />
       <section className=" ">
-	<div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-		<div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-			<img src="assets/svg/Business_SVG.svg" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
-		</div>
-		<div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-			<h1 className="text-5xl font-bold leadi sm:text-6xl">Ac mattis
-				<span className="">senectus</span>erat pharetra
-			</h1>
-			<p className="mt-6 mb-8 text-lg sm:mb-12">Dictum aliquam porta in condimentum ac integer
-				<br className="hidden md:inline lg:hidden"/>turpis pulvinar, est scelerisque ligula sem
-			</p>
-			<div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-				<a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded  ">Suspendisse</a>
-				<a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold border rounded ">Malesuada</a>
-			</div>
-		</div>
-	</div>
-</section>
-      {/* <section
-        className=" bg-cover w-10/12 mx-auto mb-10"
-        style={{
-          backgroundImage:
-            "url('https://images.unsplash.com/photo-1563986768609-322da13575f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        <div className="flex flex-col bg-black/60">
-          <div className="container flex flex-col flex-1 px-6 py-12 mx-auto">
-            <div className="w-full flex justify-center items-center">
-              <div className="md:w-10/12 px-1 md:px-8 py-10 mx-auto overflow-hidden border border-white bg-black/70 shadow-2xl rounded-xl  lg:max-w-xl">
-                
-                <h1 className="text-xl font-medium text-white text-center ">
-                  Subscribers form
-                </h1>
-                <p className="mt-2 text-white text-center ">
-                  Your fit journey starts here subscribe !
-                </p>
-                <form onSubmit={hendelSubscribe} className="mt-6">
-                  <div className="form-control">
-                    <input
-                      className="inputs inputs-alt"
-                      placeholder="Type your name"
-                      required=""
-                      type="text"
-                      name="name"
+        <div className="pb-5 lg:pb-10">
+          <div className="grid grid-cols-1 lg:grid-cols-2">
+            <div>
+              <img
+                className="w-full h-full object-cover"
+                src="https://plus.unsplash.com/premium_photo-1661347839170-85172ef52293?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fGJvZHklMjBwaG90b3xlbnwwfHwwfHx8MA%3D%3D/"
+                alt=""
+              />
+            </div>
+            <div className="bg-[#F1F5F8] overflow-hidden">
+              <div className="p-10">
+                <div className="text-center lg:text-left">
+                  <h1 className="text-xl lg:text-4xl font-semibold">
+                    What can I do with Body Pulser?
+                  </h1>
+                  <p>
+                    Streamline your hiring process with strategic channels to
+                    reach qualified candidates
+                  </p>
+                </div>
+                <br />
+                <div className="space-y-10 lg:space-y-4">
+                  <div className="flex-row text-center lg:text-left lg:flex gap-2">
+                    <img
+                      className="m-auto lg:m-0"
+                      src="https://raw.githubusercontent.com/julfiker755/Assignment11-job-stock/9e6842d94540c294630c505c7c6046fb09b03200/client/src/assets/jobtex/svg1.svg"
+                      alt=""
                     />
-                    <span className="inputs-border inputs-border-alt"></span>
+                    <div>
+                      <h1 className="text-xl font-semibold">
+                        Reduce Hiring Bias
+                      </h1>
+                      <p>
+                        Structured digital interviews increase the predictive
+                        validity of your hires
+                      </p>
+                    </div>
                   </div>
-                  <div className="form-control">
-                    <input
-                      className="inputs inputs-alt"
-                      placeholder="Type your email"
-                      required=""
-                      type="email"
-                      name="email"
+                  <div className="flex-row text-center lg:text-left lg:flex gap-2">
+                    <img
+                      className="m-auto lg:m-0"
+                      src="https://raw.githubusercontent.com/julfiker755/Assignment11-job-stock/9e6842d94540c294630c505c7c6046fb09b03200/client/src/assets/jobtex/svg2.svg"
+                      alt=""
                     />
-                    <span className="inputs-border inputs-border-alt"></span>
+                    <div>
+                      <h1 className="text-xl font-semibold">
+                        Save Time & Headspace
+                      </h1>
+                      <p>
+                        Reduce your time-to-hire by up to 80% and free up
+                        headspace for other HR priorities.
+                      </p>
+                    </div>
                   </div>
-                  {user ? (
-                    <button
-                      type="submit"
-                      className="w-full bg-[#854cff9c] mt-5 text-white text-xl space py-1 rounded-lg hover:bg-[#5B1AE9]"
-                      style={{ letterSpacing: "5px" }}
-                    >
-                      Subscribe Now
-                    </button>
-                  ) : (
-                    <button
-                      type="submit"
-                      disabled
-                      className="w-full bg-[#854cff9c] mt-5 text-white text-xl space py-1 rounded-lg hover:bg-[#5B1AE9]"
-                      style={{ letterSpacing: "5px" }}
-                    >
-                      Subscribe Now
-                    </button>
-                  )}
-                </form>
+                  <div className="flex-row text-center lg:text-left lg:flex gap-2">
+                    <img
+                      className="m-auto lg:m-0"
+                      src="https://raw.githubusercontent.com/julfiker755/Assignment11-job-stock/9e6842d94540c294630c505c7c6046fb09b03200/client/src/assets/jobtex/svg3.svg"
+                      alt=""
+                    />
+                    <div>
+                      <h1 className="text-xl font-semibold">
+                        Minimize Environmental Impact
+                      </h1>
+                      <p>
+                        Did you know? U.S. office workers use ~10,000 sheets of
+                        paper every year
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex-row text-center lg:text-left lg:flex gap-2">
+                    <img
+                      className="m-auto lg:m-0"
+                      src="https://raw.githubusercontent.com/julfiker755/Assignment11-job-stock/9e6842d94540c294630c505c7c6046fb09b03200/client/src/assets/jobtex/svg4.svg"
+                      alt=""
+                    />
+                    <div>
+                      <h1 className="text-xl font-semibold">Save Money</h1>
+                      <p>
+                        Interview more candidates, more quickly and still save
+                        money.
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </section> */}
+      </section>
+     
     </>
   );
 };
